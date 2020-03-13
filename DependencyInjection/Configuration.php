c80ce11605020c847fc9232c7cf96f0880ed02da
iangregory <iangregory@users.noreply.github.com>
2020-03-13 15:21:23 +0000

    Update TreeBuilder's root node initialization in config according to symfony deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ami_airbrake');
+        $treeBuilder = new TreeBuilder('ami_airbrake');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
